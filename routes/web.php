656aea4fdc3388e1d175e242077b8064a028022f
unknown <khinemyomyatthu24@gmail.com>
2025-07-08 22:18:22 +0630

    update userPage in user UI

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,9 @@
 <?php
 
 use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\SocialLoginController;
 use Illuminate\Support\Facades\Route;
+use Laravel\Socialite\Facades\Socialite;
 
 require_once __DIR__.'/admin.php';
 require_once __DIR__.'/user.php';
@@ -19,5 +21,10 @@ Route::middleware('auth')->group(function () {
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
+
+//googl login github login
+Route::get('/auth/{provider}/redirect',[SocialLoginController::class,'redirect']);
+
+Route::get('/auth/{provider}/callback',[SocialLoginController::class,'callback']);
 
 require __DIR__.'/auth.php';
